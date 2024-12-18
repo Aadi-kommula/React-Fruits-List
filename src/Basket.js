fdcf8f4757d4f080e7757495a5e2b3e29ffafb8f
Aadi-kommula <aadikommula@gmail.com>
2024-12-18 11:55:44 +0530

    bugs fixed

--- src/Basket.js
+++ src/Basket.js
@@ -4,19 +4,33 @@ function Basket() {
     const [fruits, setfruits]=useState(['Apple','Banana','Grapes'])
     const [msg, setmsg]=useState('')     
     function AddItem(){
-        let item=prompt('Enter the name of a fruit you would like to add to the basket.')
+        let item=document.getElementById('inp').value
+        if(item !==''){
         setfruits([...fruits,item])
+        document.getElementById('inp').value=''
+        }
+        else{
+          alert("Enter a fruit name before hitting add item")
+        }
+      
+
     }
     function RemoveItem(){
+      if(fruits.length !==0){
         var info=fruits[fruits.length-1];
         fruits.pop()
         console.log(fruits)
         setfruits([...fruits]) 
-        setmsg('Huhu you removed '+info+' from the basket')   
+        setmsg('Huhu you removed '+info+' from the basket')  
+      } 
+      else{
+        setmsg('No Fruits to remove')  
+      }
     }
        
   return (
     <>
+    <input id='inp' type='text' className='m-2' placeholder='Enter a fruit name' ></input>
     <button 
     onClick={AddItem}
     className="btn btn-success m-2">
