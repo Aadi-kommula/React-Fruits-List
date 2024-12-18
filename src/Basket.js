5114f4e9cff7ee07e50f974d703b4d893223c27e
Aadi-kommula <aadikommula@gmail.com>
2024-12-18 23:09:15 +0530

    added specific item deletion and checking case

--- src/Basket.js
+++ src/Basket.js
@@ -2,19 +2,33 @@ import React, { useState } from 'react'
 
 function Basket() {
     const [fruits, setfruits]=useState(['Apple','Banana','Grapes'])
-    const [msg, setmsg]=useState('')     
-    function AddItem(){
-        let item=document.getElementById('inp').value
+    const [msg, setmsg]=useState('')
+    
+    function AddItem() {
+      let item = document.getElementById('inp').value.trim();     
+      if (item === '') {
+        alert("Enter a fruit name before hitting add item");
+        
+      }
+    
+      let isDuplicate = false;
+      for (let x in fruits) {
+        if (fruits[x].toLowerCase() === item.toLowerCase()) {
+          isDuplicate = true;
+          break;
+        }
+      }    
+      if (isDuplicate) {
+        setmsg('Duplicates not allowed');
+      } else {
         if(item !==''){
-        setfruits([...fruits,item])
-        document.getElementById('inp').value=''
-        }
-        else{
-          alert("Enter a fruit name before hitting add item")
-        }
-      
-
+        setfruits([...fruits, item]); 
+        setmsg(`${item} added to the basket`);
+        document.getElementById('inp').value = ''; 
+      }
     }
+    }
+    
     function RemoveItem(){
       if(fruits.length !==0){
         var info=fruits[fruits.length-1];
@@ -27,7 +41,34 @@ function Basket() {
         setmsg('No Fruits to remove')  
       }
     }
-       
+    function SpecItem() {
+      var val = document.getElementById('inp1').value.trim();
+      if (fruits.length !== 0) {        
+        if (val !== '') {
+          let found = false; 
+          for (let x in fruits) {
+            if (fruits[x].toLowerCase() === val.toLowerCase()) { 
+              var index = fruits.indexOf(fruits[x]); 
+              fruits.splice(index, 1);
+              var info = val;
+              setmsg('Huhu you removed ' + info + ' from the basket');
+              setfruits([...fruits]);
+              document.getElementById('inp1').value = '';
+              found = true;
+              break;
+            }
+          }
+          if (!found) {
+            setmsg('Item not found in the basket');
+          }
+        } else {
+          setmsg('Please enter a fruit to remove');
+        }
+      } else {
+        setmsg('No Fruits to remove');
+      }
+    }
+    
   return (
     <>
     <input id='inp' type='text' className='m-2' placeholder='Enter a fruit name' ></input>
@@ -39,7 +80,13 @@ function Basket() {
     <button 
     className="btn btn-danger m-2"
     onClick={RemoveItem}>
-    Remove Item</button>
+    Remove Last Item</button><br/>
+    <input id='inp1' type='text' className='m-2' placeholder='Enter a fruit to delete' ></input>
+    <button 
+    className="btn btn-danger m-2"
+    onClick={SpecItem}>
+    Remove Specific Item</button><br/>
+
     <center>
     <p>{msg}</p>
     </center>
